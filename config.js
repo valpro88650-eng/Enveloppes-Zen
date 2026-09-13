7f6be3ff6a6c3147ae55a1bf5cf75212bfcb7e7a
valpro88650-eng <valpro88650@gmail.com>
2026-09-13 03:38:45 +0200

    config.js
    pour garder les codes et url de l'app sans avoir à les ressaisir à chaque modification du index.html

---
+++ config.js
@@ -0,0 +1,3 @@
+// Configuration Supabase - Ce fichier ne bougera plus jamais de GitHub
+const SUPABASE_URL = 'https://exgvnhsbldprvusvexqn.supabase.co'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4Z3ZuaHNibGRwcnZ1c3ZleHFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyNDg4MzYsImV4cCI6MjEwNDgyNDgzNn0.7m2Yz7y1w8HowDHgTcFOXy-dh5_WAO4MCsea6oMHJAw';
